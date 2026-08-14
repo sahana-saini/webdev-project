55060e2051a892e1a0e7fef33aa91a1d1017c716
Sahana Saini <sainisahana28@gmail.com>
2026-08-14 10:09:28 +0530

    update

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,25 @@
-alert("Hellooooo");
+// alert("Hellooooo");
+
+const users = [
+    {"name" : "John",
+    "gender" : "Male",
+    "image" : "john.png"},
+
+    {"name" : "Jane",
+    "gender" : "female",
+    "image" : "jane.png"}
+
+]
+
+var currIndex =0;
+
+function toggle(){
+    if(currIndex=== 0)
+        currIndex = 1;
+    else
+        currIndex=0;
+
+    document.getElementById("card-image").src = users[currIndex].image
+    document.getElementById("card-name").innerHTML = users[currIndex].name
+    document.getElementById("card-gender").innerHTML = users[currIndex].gender
+}
